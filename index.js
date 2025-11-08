4b8a113063212be7e07e56ece061c20a191d9bf1
George Vourkas <158522369+GeorgeVourkas@users.noreply.github.com>
2025-11-08 17:44:21 +0200

    Added animation

--- index.js
+++ index.js
@@ -28,3 +28,27 @@ if(homeButton){
   })
  
 }
+
+// Animation
+
+const scrollElements = document.querySelectorAll(".animateOnScroll");
+
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return elementTop <= (window.innerHeight || document.documentElement.clientHeight) - offset;
+};
+
+const displayScrollElement = (el) => {
+  el.classList.add("visible");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 100)) {
+      displayScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener("scroll", handleScrollAnimation);
+window.addEventListener("load", handleScrollAnimation);
